0e96d72ef2addd591a2ccd0ebb1147eac9079e78
ChikaLong <longb.0818@gmail.com>
2021-09-29 01:00:50 +0000

    [Add]メール送信機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,18 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :user_name => ENV['WELCOME_MAILER_ADDRESS'],
+      :password => ENV['WELCOME_MAILER_PASSWORD'],
+      :domain => "gmail.com",
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :authentication => :login,
+      :enable_starttls_auto => true
+    }
 
   config.web_console.whitelisted_ips = '106.72.171.160'
 end
