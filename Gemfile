baea26e624ef36bc9774c78040daeaf4075d720b
ChikaLong <longb.0818@gmail.com>
2021-09-30 12:54:03 +0000

    [Add]バッチ処理

--- Gemfile
+++ Gemfile
@@ -71,3 +71,5 @@ gem 'jquery-rails'
 gem 'font-awesome-sass','~> 5.13'
 
 gem 'dotenv-rails'
+
+gem 'whenever', require: false
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@gmail.com'
+  default from: 'from@example.com'
   layout 'mailer'
 end
